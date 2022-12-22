f49841722e208f388ae7aa52a4bb2c965dccd308
Sobirjon-Solijonov <sobirjon12345678900@gmail.com>
2022-12-22 16:45:12 +0500

    add

--- src/Companents/About/About.jsx
+++ src/Companents/About/About.jsx
@@ -6,11 +6,45 @@ import './About.scss'
 function About() {
   return (
     <>
-    <Navbar/>
-    
-    <h1>About</h1>
-    
-    <Endcard/>
+      <Navbar />
+
+      <div className="About">
+        {/* about_content */}
+        <div className="about_content">
+          <div className="about_content_bir">
+            <img src="" alt="" />
+          </div>
+          <div className="about_content_ikki">
+            <h6>WHO WE ARE</h6>
+            <h3>We combine our experience and
+              passion to develop solutions that
+              empower businesses.</h3>
+            <p>We combine our experience and
+              passion to develop solutions that
+              empower businesses.</p>
+          </div>
+        </div>
+
+
+        {/* about_name */}
+
+
+        <div className="about_name">
+          <h6>OUR SERVICES</h6>
+          <h2>Methodologies & <br />
+            Technology We Use.</h2>
+          <p>Methodologies &
+            Technology We Use.</p>
+        </div>
+
+          {/* about_huci */}
+
+          <div className="about_huci">
+            
+          </div>
+
+      </div>
+      <Endcard />
     </>
   )
 }
